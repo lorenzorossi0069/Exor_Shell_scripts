0016cdbb323a1f035dcf179421f8136947319715
Lorenzo Rossi | Exor International S.p.A. <lorenzo.rossi@exorint.com>
2025-05-16 14:25:55 +0200

    cleanup

--- P2P/p2p_common.sh
+++ P2P/p2p_common.sh
@@ -4,13 +4,14 @@
 IFACE=wlan0
 WPA_SUPPLICANT_PATH=wpa_supplicant_p2p.conf
 
+
 function p2p_cleanUp {
 	#for TEMP_IFACE in $(wpa_cli interface | grep p2p-); do
 	#    echo "Removing $TEMP_IFACE"
 	#    wpa_cli p2p_group_remove $TEMP_IFACE
 	#done
 	
-	echo "dbg: IFACE=$IFACE"
+	echo "Cleaning IFACE=$IFACE"
 	
 	wpa_cli -i $IFACE p2p_flush
 	
@@ -25,12 +26,8 @@ function p2p_killAll {
 }
 
 function p2p_startWPAsupplicant {
-	#pkill wpa_supplicant
-	#pkill hostapd
-	#sleep 2
 	p2p_killAll
 	wpa_supplicant -B -i $IFACE -c $WPA_SUPPLICANT_PATH
-
 }
 
 
